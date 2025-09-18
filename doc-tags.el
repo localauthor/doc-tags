d469c7b643b91d3fe91d81ebc46cb13e4e689d82
Grant Rosson <grantrosson@gmail.com>
2025-09-18 13:20:48 +0300

    Add package header

--- doc-tags.el
+++ doc-tags.el
@@ -1,6 +1,30 @@
 ;;; doc-tags.el --- Create sqlite database of tagged file-system documents   -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2025 Grant Rosson
+
+;; Author: Grant Rosson <https://github.com/localauthor>
+;; Created: April 17, 2025
+;; License: GPL-3.0-or-later
+;; Version: 0.1
+;; Homepage: https://github.com/localauthor/doc-tags
+;; Package-Requires: ((emacs "28.1"))
+
+;; This program is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+;; for more details.
+
+;; You should have received a copy of the GNU General Public License along
+;; with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 ;;; Commentary:
+
 
 ;;; Code:
 
